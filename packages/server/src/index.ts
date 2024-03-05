dc8635a597f5420594da0dfbaf2b5f01900ce779
Nathan Samsoedien <info@samsoedien.com>
2024-03-05 19:07:25 +0100

    chore: dockerize monorepo

---
+++ packages/server/src/index.ts
@@ -0,0 +1,17 @@
+import http from 'http'
+import dotenv from 'dotenv'
+
+import app from './api/app'
+// import connectDB from './config/mongoDB'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5001
+const server = http.createServer(app)
+
+server
+  .listen(PORT, () => {
+    // connectDB()
+    console.info(`Server running at http://localhost:${PORT}`)
+  })
+  .on('error', console.error)
