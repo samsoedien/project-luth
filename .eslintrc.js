27ad884e7ce35d0f9b49561674260a5a6243e1db
Nathan Samsoedien <info@samsoedien.com>
2024-03-08 19:19:14 +0100

    chore: re-scaffold next using t3-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,58 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: ['standard-with-typescript', 'airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  extends: [
+    "standard-with-typescript",
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser',
-    project: './tsconfig.base.json',
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
+    project: "./tsconfig.base.json",
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react'],
-  rules: {},
-}
+  plugins: ["react"],
+  rules: {
+    // These opinionated rules are enabled in stylistic-type-checked above.
+    // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+  },
+};
